3095237dba309106413924509792c46d545d2c39
igoreta <ivoronline@gmail.com>
2021-03-10 13:34:38 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_validation_requestparameters/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_validation_requestparameters/controllers/MyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Controller
 @Validated
@@ -22,7 +23,7 @@ public class MyController {
   @RequestMapping("/Hello")
   public String hello(
     @RequestParam @NotBlank String  name,
-    @RequestParam @NotBlank Integer age
+    @RequestParam @NotNull  Integer age
   ) {
     return "Hello " + name;
   }
